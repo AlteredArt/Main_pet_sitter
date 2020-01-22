db1799e8c0d98798ec80c9d2716c1e2d32d2e11b
Altered Art <cherokeedreemer@gmail.com>
2020-01-22 16:07:57 -0700

    helpus

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ Pet.destroy_all
 
 
 
-owner1 = Owner.create(:first_name "Doug", :last_name "billings")
-owner2 = Owner.create(:first_name "John", :last_name "Smith")
-owner3 = Owner.create(:first_name "Bill", :last_name "White")
-owner1 = Owner.create(:first_name "Tom", :last_name "Hanks")
+owner1 = Owner.create(first_name: "Doug", last_name: "billings")
+owner2 = Owner.create(first_name: "John", last_name: "Smith")
+owner3 = Owner.create(first_name: "Bill", last_name: "White")
+owner1 = Owner.create(first_name: "Tom", last_name: "Hanks")
 
 
 pet1 = Pet.create(name: "waffles",kind: "cat",age: 11)
@@ -16,10 +16,9 @@ pet3 = Pet.create(name: "harley",kind: "dog",age: 9)
 pet4 = Pet.create(name: "waffle",kind: "cat",age: 7)
 pet5 = Pet.create(name: "cookie",kind: "cat",age: 6)
 
-sitter1 = Sitter.create("Walk a Dog")
-sitter2 = Sitter.create("Pet a Cat")
-sitter3 = Sitter.create("Doggie Days")
-sitter4 = Sitter.create("Feed a Cat")
+sitter1 = Sitter.create(name_of_company: "Walk a Dog")
+sitter2 = Sitter.create(name_of_company: "Pet a Cat")
+sitter3 = Sitter.create(name_of_company: "Doggie Days")
+sitter4 = Sitter.create(name_of_company: "Feed a Cat")
 
-appointment1 = Appointment.create(date: date, pet: "Waffles",sitter: "Walk the Dog")
-pet1.appointments.create(date: date)
+appointment1 = Appointment.create(date: , pet: "Waffles",sitter: "Walk the Dog")
--- runner.rb
+++ runner.rb
@@ -1,42 +1,6 @@
-require_relative config/environment
+require_relative "config/environment"
 
-# “Welcome to /"Pet Sitter/” have you used our app before?”
-#         if yes proceed to create a appointment
-#             if no create a customer
-#                 then create a pet
-
-#         (if yes, or after creation)
-#             create a appointment(date, pet, sitter)
-
-
-def welcome
-    puts "Welcome to 'Pet Sitter” have you used our app before?"
-    puts "Enter: Yes or No"
-end
-
-def prompt_owner
-    user_input = gets.chomp
-end
-
-def answer?
-    prompt_owner
-    owner_input = prompt_owner
-    if owner_input == "yes"
-      puts "would you like to create a appointment?"
-    elsif owner_input = "no"
-      puts "would you like to create a new owner"
-    else
-      puts invalid_command
-    end
-  end
-end
-
-
-
-def the_end
-    puts "thanks for using our app"
-end
-
-
+cli = Cli.new
+cli.welcome_menu
 
 
