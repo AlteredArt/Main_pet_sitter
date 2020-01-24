20638878b5126fc50e79795845e2f389a70273e1
Altered Art <cherokeedreemer@gmail.com>
2020-01-24 13:20:24 -0700

    final

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -1,12 +1,13 @@
 class Cli
     attr_reader :owner
 
+
 def welcome
     puts "Welcome, Please enter your first name"
         user_first = gets.chomp
     puts "Please enter your last name"
         user_last = gets.chomp
-        puts "Welcome, #{full_name(user_first, user_last)}"
+            puts "Welcome, #{full_name(user_first, user_last)}"
         find_owner(user_first, user_last)
     end
 end
@@ -15,15 +16,14 @@ def full_name(user_first, user_last)
     "#{user_first} #{user_last}"
 end  
 
-
 def find_owner(user_first, user_last)
         @owner = Owner.find_by(first_name: user_first,last_name: user_last)
      if owner
-        puts "Here are your furballs:"
-        puts owner.pets.pluck(:name)  #change eventually
-        menu
+        sub_menu
     else
          create_a_owner(user_first, user_last)
+       
+         create_new_pet
     end
 end
 
@@ -32,51 +32,78 @@ def create_a_owner(user_first, user_last)
  end
 
 def menu
-       puts "Would you like to 1.) Add a new pet, or 2.) Add a appointment for a current pet?"
-        puts"Enter 'New Pet' or 'New Appointment'"
+       puts "Would you like to add a new pet? Enter '1' \n Would you like to setup a appointment for a current pet? Enter '2' \n Or would you like to exit? Enter '3'"
+       
         user_answer = gets.chomp.downcase
-            if user_answer == "new pet"
+            if user_answer == "1"
                 create_new_pet
-            else user_answer == "new appointment"
-                create_new_appointmet
+            elsif user_answer == "2"
+                sitters_menu
+            else user_answer == "3"
+            end_program    
         end
-
     end
 
-#   def create_new_pet
+    def sub_menu
+        puts "Here are your furballs:"
+        puts owner.pets.pluck(:name) 
+        menu
+    end
 
-#       Pet.create(name: "name", kind: "kind", age: age)
-#   end
+    def create_new_pet
+        puts "Please enter your pets name"
+            pets_name = gets.chomp.downcase
+        puts "Please enter what kind of pet"
+            pets_kind = gets.chomp.downcase
+        puts "Please enter your pets age"
+            pets_age = gets.chomp.downcase
+    owner.pets.create(name: pets_name, kind: pets_kind, age: pets_age)
+        puts "Congratulations you made a new pet"
+        sub_menu
+    end
+    
+ def sitters_menu
+        puts Sitter.all.pluck(:name_of_company).uniq
+        puts "Which sitter would you like to schedule with?"
+            sitter_answer = gets.chomp.downcase
+            selected_sitter = Sitter.all.find do |sitter|
+                sitter.name_of_company == sitter_answer
+      end
+        puts "which pet would pet would you like to schedule with?"
+            pet_answer = gets.chomp.downcase
+            selected_pet = Pet.all.find do |pet|
+                pet.name == pet_answer
+    end
 
-# def create_new_owner(first_name, last_name)
-#     Owner.create(first_name: "first_name", last_name: "last_name")
-# end
+         puts "what date would work for you?"
+            date_answer = gets.chomp
+            create_new_appointment(date_answer, selected_pet, selected_sitter)
+            puts "Congratulations you made a new appointment"
+             menu
+    end
 
 
-# def next_step
-#     puts "Welcome, #{owner}, would you like set up a appointment for {pet}?"
-#     puts "Enter, Yes or No"
-#     user_answer == gets.chomp.downcase
-#         if user_answer == "Yes"
-#             create_new_appointmet
-#         elsif user_answer == "No"
-#             end_program
-#     end
-#     end
-#     end
+    
+def create_new_appointment(date_answer, pet_answer, sitter_answer)
+                                # data types and arguments
+        Appointment.create(date: date_answer, pet: pet_answer, sitter: sitter_answer)
+                                 # data types and arguments
+        puts "Congratulations you made a new appointment"
+            end_menu
+        end
+    
+def end_menu
+    puts "Enter 1.) for Start Menu or 2.) To Quit"
+        end_answer = gets.chomp.downcase
+            if end_answer == "1"
+                sub_menu
+            else end_answer == "2"
+                end_program
+    end
+end
 
-# def end_program
-#     puts "Thaks for using our app, GoodBye."
-
-# def prompt_owner
-#     user_input = gets.chomp
-# end
-
-# def create_a_owner
-#     Owner.create(first_name: "first_name", last_name: "last_name")
-# end
-
-# def create_a_pet
-#     Pet.create(name: "name", kind: "kind", age: age)
-# end
+def end_program
+     puts "Thaks for using our app, Enjoy."
+        exit!
+ end
 
--- lib/models/pet.rb
+++ lib/models/pet.rb
@@ -2,6 +2,7 @@ class Pet < ActiveRecord::Base
     has_many :appointments
     has_many :sitters, through: :appointments
     belongs_to :owner
-   
+
+
 end
 
